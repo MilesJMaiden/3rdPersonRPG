4dc5b127ef127452f3ec7b932d8cc2e3f362262f
BattlestationX2\MJM <MilesJMaiden@Gmail.com>
2023-03-18 13:06:31 +0000

    Inputs can now drive statemachine

--- 3rdPersonRPG/Assets/Scripts/StateMachine/Player/PlayerStateMachine.cs
+++ 3rdPersonRPG/Assets/Scripts/StateMachine/Player/PlayerStateMachine.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerStateMachine : StateMachine //Right Side of the colon is what you inherit from
 {
+
+    [field: SerializeField] public InputReader InputReader { get; private set; } //rules get - you can publicly get the input reader. private set - ONLY privately can this be set // //Pascal case
+
     // Start is called before the first frame update
     void Start()
     {
--- 3rdPersonRPG/Assets/Scripts/StateMachine/Player/PlayerTestState.cs
+++ 3rdPersonRPG/Assets/Scripts/StateMachine/Player/PlayerTestState.cs
@@ -7,29 +7,34 @@ using UnityEngine.Rendering;
 //This will be the framework for every state and this is where I will write the player logic
 public class PlayerTestState : PlayerBaseState
 {
-    private float timer = 5f;
+    private float timer;
     public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine) //call the base constructor passing in the needed state machine and caching it 
     {
     }
 
     public override void Enter()
     {
-        Debug.Log("Enter");
+        //Debug.Log("Enter");
+        stateMachine.InputReader.jumpEvent += OnJump; //Subscribe
     }
+
+
     public override void Tick(float deltaTime)
     {
-        timer -= deltaTime;
+        timer += deltaTime;
 
         Debug.Log(timer);
 
-        if(timer <= 0f)
-        {
-            stateMachine.SwitchState(new PlayerTestState(stateMachine));
-        }
     }
 
     public override void Exit()
     {
-        Debug.Log("Exit");
+        //Debug.Log("Exit");
+        stateMachine.InputReader.jumpEvent -= OnJump; //UNSubscribe
+    }
+
+    private void OnJump()
+    {
+        stateMachine.SwitchState(new PlayerTestState(stateMachine));
     }
 }
