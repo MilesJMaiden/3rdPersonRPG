827b3416c91964697863c55adbb5c199cd4f2675
BattlestationX2\MJM <MilesJMaiden@Gmail.com>
2023-03-24 13:08:18 +0000

    EnemyIdleState (+animation)

---
+++ 3rdPersonRPG/Assets/Scripts/StateMachine/Enemy/EnemyIdleState.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyIdleState : EnemyBaseState
+{
+
+    private readonly int locomotionBlendTreeHash = Animator.StringToHash("Locomotion");
+
+    private readonly int speedHash = Animator.StringToHash("Speed");
+
+    private const float crossFadeDuration = 0.1f;
+    
+    private const float animatorDampTime = 0.1f;
+    public EnemyIdleState(EnemyStateMachine stateMachine) : base(stateMachine){}
+
+    public override void Enter()
+    {
+        stateMachine.animator.CrossFadeInFixedTime(locomotionBlendTreeHash, crossFadeDuration);
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        stateMachine.animator.SetFloat(speedHash, 0f, animatorDampTime, deltaTime);
+    }
+
+    public override void Exit(){}
+}
--- 3rdPersonRPG/Assets/Scripts/StateMachine/Enemy/EnemyStateMachine.cs
+++ 3rdPersonRPG/Assets/Scripts/StateMachine/Enemy/EnemyStateMachine.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 
 public class EnemyStateMachine : StateMachine
 {
+    [field: SerializeField] public Animator animator { get; private set; }
 
+    void Start()
+    {
+        SwitchState(new EnemyIdleState(this));
+    }
 }
