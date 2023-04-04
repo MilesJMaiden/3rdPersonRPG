eae524356fe1ddcd3fd32156ce24c13986089d76
BattlestationX2\MJM <MilesJMaiden@Gmail.com>
2023-04-04 15:58:48 +0100

    Added ledge detection script and set up player collider to trigger

--- 3rdPersonRPG/Assets/Scripts/StateMachine/Player/PlayerStateMachine.cs
+++ 3rdPersonRPG/Assets/Scripts/StateMachine/Player/PlayerStateMachine.cs
@@ -21,6 +21,8 @@ public class PlayerStateMachine : StateMachine //Right Side of the colon is what
 
     [field: SerializeField] public Ragdoll ragdoll { get; private set; }
 
+    [field: SerializeField] public LedgeDetector LedgeDetector { get; private set; }
+
     [field: SerializeField] public float freeLookMovementSpeed { get; private set; }
 
     [field: SerializeField] public float targetingMovementSpeed { get; private set; }
